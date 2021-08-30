97c00454949506d8d3e833c227c736a0f7953325
iayushbhartiya <79574192+iayushbhartiya@users.noreply.github.com>
2021-08-30 21:20:20 +0530

    Create Faulty calculator.py

---
+++ Faulty calculator.py
@@ -0,0 +1,21 @@
+x = int(input("enter 1st number: "))
+y = int(input("enter 2nd number: "))
+opt= input("enter your operator")
+if opt=='+':
+    if x== 56 and y== 9:
+        print("77")
+    else:
+     a = x+y
+     print(a)
+if opt=="*":
+    if x==45 and y==3:
+        print("555")
+    else:
+        m = x*y
+        print(m)
+if opt=="/":
+    if x==56 and y==6:
+        print(4)
+    else:
+        d = x/y
+        print(d)
